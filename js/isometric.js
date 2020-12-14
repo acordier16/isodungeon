c8b65046295ed1c47c989bd24e04de5f807289b1
acordier <acordier$@scortex.io>
2020-12-14 13:36:26 +0100

    isometric: remove tile cursor out of map, isodungeon: organize files in folders

---
+++ js/isometric.js
@@ -0,0 +1,284 @@
+// A simple isometric tile renderer
+export class Isometric {
+    constructor() {
+        this.tileColumnOffset = 100; // pixels
+        this.tileRowOffset = 50; // pixels
+        this.originX = 0; // offset from left
+        this.originY = 0; // offset from top
+        this.Xtiles = 0; // Number of tiles in X-dimension
+        this.Ytiles = 0; // Number of tiles in Y-dimension
+        this.selectedTileX = 0;
+        this.selectedTileY = 0;
+        this.context = undefined;
+        this.canvas = undefined;
+        this.graph = undefined;
+        this.tileImages = undefined;
+        this.showCoordinates = false;
+    }
+
+    load(map) {
+        this.canvas = $("#isocanvas");
+        this.context = this.canvas[0].getContext("2d");
+        this.Xtiles = map.mapArray.length;
+        this.Ytiles = map.mapArray[0].length;
+        this.tileImages = new Array();
+        var loadedImages = 0;
+        var totalImages = map.tiles.length;
+        // Load all the images before we run the app
+        for (var i = 0; i < map.tiles.length; i++) {
+            this.tileImages[i] = new Image();
+            //this.tileImages[i].onload = function() {
+            //    if (++loadedImages >= totalImages) {
+            //        self.run(entities, buttons, phase);
+            //    }
+            //};
+            this.tileImages[i].src = map.tiles[i];
+        }
+    }
+
+    drawEntities(entities) {
+        for (var i = 0; i < entities.length; i++) {
+            this.drawEntity(entities[i]);
+        }
+    }
+
+    drawEntity(entity) {
+        var offX =
+            (entity.x * this.tileColumnOffset) / 2 +
+            (entity.y * this.tileColumnOffset) / 2 +
+            this.originX +
+            entity.sprite.width / 2;
+        var offY =
+            (entity.y * this.tileRowOffset) / 2 -
+            (entity.x * this.tileRowOffset) / 2 +
+            this.originY -
+            entity.sprite.height / 2 -
+            this.tileRowOffset / 2;
+        this.context.drawImage(entity.sprite, offX, offY);
+    }
+
+    drawButtons(buttons) {
+        for (var i = 0; i < buttons.length; i++) {
+            this.drawButton(buttons[i]);
+        }
+    }
+
+    drawButton(button) {
+        var off = this.isometricToCartesian(button.x, button.y);
+        var offX = off[0];
+        var offY = off[1];
+        button.offX = offX - button.sprite.width / 2 + this.tileColumnOffset / 2;
+        button.offY = offY - button.sprite.height / 2 + this.tileRowOffset / 2;
+        this.context.drawImage(button.sprite, button.offX, button.offY);
+        this.context.font = button.font;
+        this.context.fillStyle = button.color;
+        this.context.textAlign = "center";
+        this.context.fillText(
+            button.text,
+            button.offX + button.sprite.width / 2,
+            button.offY + button.sprite.height / 2 + 5
+        );
+    }
+
+    // to put in utils
+    entityAtThisPosition(x, y, entities) {
+        for (var i = 0; i < entities.length; i++) {
+            if (x == entities[i].x && y == entities[i].y) {
+                return entities[i];
+            }
+        }
+        return null;
+    }
+
+    // to put in entity.js
+    tileWithinEntityRange(x, y, entity) {
+        if (Math.abs(entity.x - x) + Math.abs(entity.y - y) <= entity.PM) {
+            return true;
+        }
+        return false;
+    }
+
+    // to put in action.js
+    tileWithinActionRange(x, y, entity, action) {
+        var distanceX = Math.abs(entity.x - x);
+        var distanceY = Math.abs(entity.y - y);
+        if (action.rangeType == "diamond") {
+            if (action.minPO <= distanceX + distanceY && distanceX + distanceY <= action.maxPO) {
+                return true;
+            }
+        } else if (action.rangeType == "cross") {
+            if (
+                (distanceX == 0 && action.minPO <= distanceY && distanceY <= action.maxPO) ||
+                (distanceY == 0 && action.minPO <= distanceX && distanceX <= action.maxPO)
+            ) {
+                return true;
+            }
+        } else if (action.rangeType == "circle") {
+            if (
+                Math.pow(action.minPO, 2) <= Math.pow(distanceX, 2) + Math.pow(distanceY, 2) &&
+                Math.pow(distanceX, 2) + Math.pow(distanceY, 2) <= Math.pow(action.maxPO, 2)
+            ) {
+                return true;
+            }
+        }
+        // TO DO: square
+        return false;
+    }
+
+    // not use map but rather dimensions of isometric scene
+    drawPODiamondsForAction(entity, action, map) {
+        for (var Xi = 0; Xi < map.mapArray.length; Xi++) {
+            for (var Yi = 0; Yi < map.mapArray[0].length; Yi++) {
+                if (this.tileWithinActionRange(Xi, Yi, entity, action)) {
+                    this.drawDiamond(Xi, Yi, "blue", true, "blue", 0.5);
+                }
+            }
+        }
+    }
+
+    // not use map but rather dimensions of isometric scene
+    drawPMDiamondsForEntity(entity, map) {
+        for (var Xi = 0; Xi < map.mapArray.length; Xi++) {
+            for (var Yi = 0; Yi < map.mapArray[0].length; Yi++) {
+                var path = map.pathFromEntityToTile(Xi, Yi, entity);
+                if (path.length > 0 && path.length <= entity.PM) {
+                    this.drawDiamond(Xi, Yi, "orange", true, "orange", 0.5);
+                }
+            }
+        }
+    }
+
+    isometricToCartesian(x, y) {
+        return [
+            (x * this.tileColumnOffset) / 2 + (y * this.tileColumnOffset) / 2 + this.originX,
+            (y * this.tileRowOffset) / 2 - (x * this.tileRowOffset) / 2 + this.originY,
+        ];
+    }
+
+    displayTextTopLeft(text) {
+        this.context.fillStyle = "white";
+        this.context.font = "14pt Arial";
+        this.context.fillText(text, 20, 30);
+    }
+
+    updateCanvasSize() {
+        var width = $(window).width();
+        var height = $(window).height() - 120;
+
+        this.context.canvas.width = width;
+        this.context.canvas.height = height;
+
+        this.originX = width / 2 - (this.Xtiles * this.tileColumnOffset) / 2;
+        this.originY = height / 2;
+    }
+
+    redrawTiles(entities, targetTileColor, map) {
+        this.context.canvas.width = this.context.canvas.width;
+
+        for (var Xi = this.Xtiles - 1; Xi >= 0; Xi--) {
+            for (var Yi = 0; Yi < this.Ytiles; Yi++) {
+                var imageIndex = map.mapArray[Xi][Yi];
+                this.drawTile(Xi, Yi, imageIndex);
+            }
+        }
+        if (this.isCursorOnMap()) {
+            this.drawDiamond(this.selectedTileX, this.selectedTileY, targetTileColor, true, targetTileColor, 0.75);
+            this.context.fillStyle = "yellow";
+            var idx = map.mapArray[this.selectedTileX][this.selectedTileY];
+            this.context.font = "14pt Arial";
+            var entity = this.entityAtThisPosition(this.selectedTileX, this.selectedTileY, entities);
+            if (entity != null) {
+                this.context.fillText(
+                    entity.name.concat(
+                        ", ",
+                        entity.basePV,
+                        "(+",
+                        entity.PV - entity.basePV,
+                        ")/",
+                        entity.initialPV,
+                        " PV, ",
+                        entity.basePA,
+                        "(+",
+                        entity.PA - entity.basePA,
+                        ")/",
+                        entity.initialPA,
+                        " PA, ",
+                        entity.basePM,
+                        "(+",
+                        entity.PM - entity.basePM,
+                        ")/",
+                        entity.initialPM,
+                        " PM"
+                    ),
+                    20,
+                    60
+                );
+            }
+        }
+    }
+
+    isCursorOnMap() {
+        return (
+            this.selectedTileX >= 0 &&
+            this.selectedTileX < this.Xtiles &&
+            this.selectedTileY >= 0 &&
+            this.selectedTileY < this.Ytiles
+        );
+    }
+
+    drawTile(Xi, Yi, imageIndex) {
+        var offX = (Xi * this.tileColumnOffset) / 2 + (Yi * this.tileColumnOffset) / 2 + this.originX;
+        var offY = (Yi * this.tileRowOffset) / 2 - (Xi * this.tileRowOffset) / 2 + this.originY;
+
+        this.context.drawImage(this.tileImages[imageIndex], offX, offY);
+
+        if (this.showCoordinates) {
+            this.context.fillStyle = "orange";
+            this.context.fillText(
+                Xi + ", " + Yi,
+                offX + this.tileColumnOffset / 2 - 9,
+                offY + this.tileRowOffset / 2 + 3
+            );
+        }
+    }
+
+    drawDiamond(Xi, Yi, color, fill = false, fillColor = undefined, fillAlpha = undefined) {
+        var offX = (Xi * this.tileColumnOffset) / 2 + (Yi * this.tileColumnOffset) / 2 + this.originX;
+        var offY = (Yi * this.tileRowOffset) / 2 - (Xi * this.tileRowOffset) / 2 + this.originY;
+
+        color = typeof color !== "undefined" ? color : "white";
+        this.context.strokeStyle = color;
+        this.context.beginPath();
+        this.context.moveTo(offX, offY + this.tileRowOffset / 2, offX + this.tileColumnOffset / 2, offY, color);
+        this.context.lineTo(
+            offX + this.tileColumnOffset / 2,
+            offY,
+            offX + this.tileColumnOffset,
+            offY + this.tileRowOffset / 2,
+            color
+        );
+        this.context.lineTo(
+            offX + this.tileColumnOffset,
+            offY + this.tileRowOffset / 2,
+            offX + this.tileColumnOffset / 2,
+            offY + this.tileRowOffset,
+            color
+        );
+        this.context.lineTo(
+            offX + this.tileColumnOffset / 2,
+            offY + this.tileRowOffset,
+            offX,
+            offY + this.tileRowOffset / 2,
+            color
+        );
+        this.context.closePath();
+        if (fill) {
+            this.context.globalAlpha = fillAlpha;
+            this.context.fillStyle = fillColor;
+            this.context.fill();
+        }
+        this.context.stroke();
+        this.context.lineWidth = 2;
+        this.context.globalAlpha = 1;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,45 @@
+import { Game } from "/game.js";
+import { Isometric } from "/isometric.js";
+import { Entity } from "/entities.js";
+import { Action, Effect } from "/actions.js";
+import { Map } from "/maps.js";
+var isometric = new Isometric();
+// TO DO: make this a dico 0: "tile.png"?
+var tiles = [
+    "images/land.png", //0
+    "images/water.png", //1
+];
+var map = [
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+];
+var blockingIds = [1];
+var map = new Map(tiles, map, blockingIds);
+var action1 = new Action(
+    "Corps-à-corps",
+    2,
+    "cross",
+    1,
+    1,
+    // costPA, POmin, POmax
+    [new Effect("definitive", 1, true, 1, -1, 0, 0, 0)]
+);
+// type, duration, allowMultiple, probability, PV, PM, PA, PO
+var action2 = new Action("Ralentissement", 2, "diamond", 3, 5, [new Effect("temporary", 3, true, 0, 0, 0, -2, 0)]);
+var action3 = new Action("Poison", 2, "diamond", 3, 5, [new Effect("definitive", 3, false, 0, -5, 0, 0, 0)]);
+
+var hero = new Entity(1, 4, "hero", true, "images/hero.png", 60, 6, 3, 0, [action1, action2, action3]);
+var enemy = new Entity(2, 4, "monster", false, "images/hero.png", 20, 6, 2, 0, []);
+var entities = [hero, enemy];
+//var endOfTurnButton = new Button(-2, 11, "End of turn", "images/uipack-rpg/PNG/buttonLong_blue.png", "images/uipack-rpg/PNG/buttonLong_blue_pressed.png");
+var buttons = [];
+var game = new Game(isometric, entities, buttons, map);
+game.load();
